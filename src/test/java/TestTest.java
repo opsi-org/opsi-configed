eab1fbf271febb9bb19c076ebf612beb79efe58a
Nils Otto <>
2022-10-27 19:21:16 +0200

    Die Resourcen des configed wurden alle hinzugefügt in /src/main/resources (der Maven Standardpfad dafür). Die Abhängigkeiten des configed werden nun - wie in der pom.xml definiert - geladen, anstatt dass sie lokal vorliegen. Sie werden bei 'package' in /target/lib kopiert und im Manifest der jar-Datei werden sie in Class-Path entsprechend verlinkt, sodass nach einem 'mvn package' die jar-Datei direkt ausgeführt werden und der configed gestartet werden kann. Dadurch konnten die lokalen lib-Dateien gelöscht werden und Maven liefert keine Warnungen mehr beim bei dem ganzen (abgesehen von Code-bedingten Warnungen beim Kompilieren).

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class TestTest {
+ 
+    @Test
+    void erfolgreicherTest() {
+		assertTrue(true);
+    }
+}
