dfc37f6131a9e9d06601b54ec8ddd4a7a1065da0
Nils Otto <>
2022-11-02 16:41:47 +0100

    pom.xml angepasst

--- src/test/java/properties/TestTest.java
+++ src/test/java/properties/TestTest.java
@@ -1,3 +1,5 @@
+package properties;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -7,11 +9,14 @@ import java.util.Iterator;
 import java.util.Properties;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import de.uib.messages.Messages;
 
 
 class TestTest {
+
+  Logger log = Logger.getGlobal();
 
   /**
    * Testet, ob die Übersetzungsdateien vollständig sind in dem Sinne, dass
@@ -22,17 +27,16 @@ class TestTest {
     void testIfEverythingTranslated() {
 	  	assertTrue(true);
 
-      Logger log = Logger.getGlobal();
-
       Properties properties = new Properties();
       InputStream is = Messages.class.getResourceAsStream("valid_localisations.conf");
 
       try {
         properties.load(is);
-      } catch (Exception e) {}
+      } catch (Exception e) {
+        assertTrue(false, "Fehler beim laden von 'valid_localisations.conf'");
+      }
       
       Set<String> keySet = properties.stringPropertyNames();
-      log.info(keySet.toString());
       
       Iterator<String> iterator = keySet.iterator();
 
@@ -60,7 +64,6 @@ class TestTest {
      * @param second zweite Sprache (Kürzel)
      */
     void compareTwoLocals(String first, String second) {
-      Logger log = Logger.getGlobal();
 
       String firstFileName = "configed_" + first + ".properties";
       String secondFileName = "configed_" + second + ".properties";
@@ -85,13 +88,13 @@ class TestTest {
         assertTrue(false);
       }
 
-      Set<String> firstMinusSecond = new HashSet(firstProperties.keySet());
-      firstMinusSecond.removeAll(secondProperties.keySet());
+      Set<String> firstMinusSecond = new HashSet<>(firstProperties.stringPropertyNames());
+      firstMinusSecond.removeAll(secondProperties.stringPropertyNames());
 
       assertTrue(firstMinusSecond.isEmpty(), "Folgende properties fehlen in " + secondFileName + ":\n" + firstMinusSecond.toString());
 
-      Set<String> secondMinusFirst = new HashSet(secondProperties.keySet());
-      secondMinusFirst.removeAll(firstProperties.keySet());
+      Set<String> secondMinusFirst = new HashSet<>(secondProperties.stringPropertyNames());
+      secondMinusFirst.removeAll(firstProperties.stringPropertyNames());
 
       assertTrue(secondMinusFirst.isEmpty(), "Folgende properties fehlen in " + firstFileName + ":\n" + secondMinusFirst.toString());
     }
