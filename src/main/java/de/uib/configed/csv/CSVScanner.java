f18e4bbbf908c8f941df956d89fb26e5c2831442
Nils Otto <>
2022-11-18 15:10:43 +0100

    Some imports removed

--- src/main/java/de/uib/configed/csv/CSVScanner.java
+++ src/main/java/de/uib/configed/csv/CSVScanner.java
@@ -1,160 +1,144 @@
 package de.uib.configed.csv;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
-import de.uib.configed.csv.exceptions.*;
+public class CSVScanner {
+	private static final CSVFormat DEFAULT_FORMAT = new CSVFormat();
 
-public class CSVScanner
-{
-    private static final CSVFormat DEFAULT_FORMAT = new CSVFormat();
+	private CSVFormat format;
+	private BufferedReader reader;
 
-    private CSVFormat format;
-    private BufferedReader reader;
+	private StringBuilder field = new StringBuilder();
+	private List<List<CSVToken>> tokens = new ArrayList<>();
+	private boolean inQuotes = false;
+	private boolean isMultiLine = false;
+	private boolean isFieldWithEmbeddedDoubleQuotes = false;
 
-    private StringBuilder field = new StringBuilder();
-    private List<List<CSVToken>> tokens = new ArrayList<>();
-    private boolean inQuotes = false;
-    private boolean isMultiLine = false;
-    private boolean isFieldWithEmbeddedDoubleQuotes = false;
+	public CSVScanner(Reader reader) {
+		this(reader, DEFAULT_FORMAT);
+	}
 
-    public CSVScanner(Reader reader)
-    {
-        this(reader, DEFAULT_FORMAT);
-    }
+	public CSVScanner(Reader reader, CSVFormat format) {
+		this.reader = (reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader));
+		this.format = format;
+	}
 
-    public CSVScanner(Reader reader, CSVFormat format)
-    {
-        this.reader = (reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader));
-        this.format = format;
-    }
+	public void scan() throws IOException {
+		boolean skip = false;
+		String currentLine = null;
+		boolean fieldBeginsWithEmbeddedQuote = false;
 
-    public void scan() throws IOException
-    {
-        boolean skip = false;
-        String currentLine = null;
-        boolean fieldBeginsWithEmbeddedQuote = false;
+		while ((currentLine = reader.readLine()) != null) {
+			List<CSVToken> lineOfTokens = new ArrayList<>();
+			CharReader cReader = new CharReader(currentLine.toCharArray(), currentLine.length());
 
-        while ((currentLine = reader.readLine()) != null)
-        {
-            List<CSVToken> lineOfTokens = new ArrayList<>();
-            CharReader cReader = new CharReader(currentLine.toCharArray(), currentLine.length());
+			for (int i = 0; i < cReader.size(); i++) {
+				char c = cReader.consume();
 
-            for (int i = 0; i < cReader.size(); i++)
-            {
-                char c = cReader.consume();
+				if (skip) {
+					skip = false;
+					continue;
+				}
 
-                if (skip)
-                {
-                    skip = false;
-                    continue;
-                }
+				if (cReader.peek() == format.getFieldSeparator()) {
+					if (!field.toString().trim().isEmpty()) {
+						if (c != format.getStringSeparator() && c != format.getFieldSeparator()) {
+							field.append(c);
+							c = '\0';
+						}
+						CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
+						lineOfTokens.add(fieldToken);
+						field.setLength(0);
+					}
+				}
 
-                if (cReader.peek() == format.getFieldSeparator())
-                {
-                    if (!field.toString().trim().isEmpty())
-                    {
-                        if (c != format.getStringSeparator() && c != format.getFieldSeparator())
-                        {
-                            field.append(c);
-                            c = '\0';
-                        }
-                        CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
-                        lineOfTokens.add(fieldToken);
-                        field.setLength(0);
-                    }
-                }
+				if (c == format.getStringSeparator()) {
+					if ((cReader.peek() == format.getFieldSeparator() || i == currentLine.length() - 1)
+							&& fieldBeginsWithEmbeddedQuote) {
+						fieldBeginsWithEmbeddedQuote = false;
+						CSVToken emptyFieldToken = new CSVToken(CSVToken.EMPTY_FIELD,
+								String.format("%c%c", format.getStringSeparator(), format.getStringSeparator()));
+						lineOfTokens.add(emptyFieldToken);
+						continue;
+					}
 
-                if (c == format.getStringSeparator())
-                {
-                    if ((cReader.peek() == format.getFieldSeparator() || i == currentLine.length() - 1) && fieldBeginsWithEmbeddedQuote)
-                    {
-                        fieldBeginsWithEmbeddedQuote = false;
-                        CSVToken emptyFieldToken = new CSVToken(CSVToken.EMPTY_FIELD, String.format("%c%c", format.getStringSeparator(), format.getStringSeparator()));
-                        lineOfTokens.add(emptyFieldToken);
-                        continue;
-                    }
+					if ((cReader.lastRead() == format.getStringSeparator() && i == 0
+							|| cReader.lastRead() == format.getFieldSeparator())
+							&& cReader.peek() == format.getStringSeparator()) {
+						fieldBeginsWithEmbeddedQuote = true;
+						continue;
+					}
 
-                    if ((cReader.lastRead() == format.getStringSeparator() && i == 0 || cReader.lastRead() == format.getFieldSeparator()) && cReader.peek() == format.getStringSeparator())
-                    {
-                        fieldBeginsWithEmbeddedQuote = true;
-                        continue;
-                    }
+					if (cReader.peek() == format.getStringSeparator() && i != currentLine.length() - 1) {
+						if (fieldBeginsWithEmbeddedQuote) {
+							CSVToken stringSeparatorToken = new CSVToken(CSVToken.STRING_SEPARATOR,
+									String.valueOf(format.getStringSeparator()));
+							lineOfTokens.add(stringSeparatorToken);
+							CSVToken embeddedQuoteToken = new CSVToken(CSVToken.EMBEDDED_QUOTE,
+									String.valueOf(format.getStringSeparator()));
+							lineOfTokens.add(embeddedQuoteToken);
+							fieldBeginsWithEmbeddedQuote = false;
 
-                    if (cReader.peek() == format.getStringSeparator() && i != currentLine.length() - 1)
-                    {
-                        if (fieldBeginsWithEmbeddedQuote)
-                        {
-                            CSVToken stringSeparatorToken = new CSVToken(CSVToken.STRING_SEPARATOR, String.valueOf(format.getStringSeparator()));
-                            lineOfTokens.add(stringSeparatorToken);
-                            CSVToken embeddedQuoteToken = new CSVToken(CSVToken.EMBEDDED_QUOTE, String.valueOf(format.getStringSeparator()));
-                            lineOfTokens.add(embeddedQuoteToken);
-                            fieldBeginsWithEmbeddedQuote = false;
+							inQuotes = !inQuotes;
+						} else {
+							CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
+							lineOfTokens.add(fieldToken);
+							field.setLength(0);
+							CSVToken embeddedQuoteToken = new CSVToken(CSVToken.EMBEDDED_QUOTE,
+									String.valueOf(format.getStringSeparator()));
+							lineOfTokens.add(embeddedQuoteToken);
+						}
 
-                            inQuotes = !inQuotes;
-                        }
-                        else
-                        {
-                            CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
-                            lineOfTokens.add(fieldToken);
-                            field.setLength(0);
-                            CSVToken embeddedQuoteToken = new CSVToken(CSVToken.EMBEDDED_QUOTE, String.valueOf(format.getStringSeparator()));
-                            lineOfTokens.add(embeddedQuoteToken);
-                        }
+						skip = true;
+						continue;
+					}
 
-                        skip = true;
-                        continue;
-                    }
+					inQuotes = !inQuotes;
 
-                    inQuotes = !inQuotes;
+					if (!field.toString().trim().isEmpty()) {
+						CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
+						lineOfTokens.add(fieldToken);
+						field.setLength(0);
+					}
 
-                    if (!field.toString().trim().isEmpty())
-                    {
-                        CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
-                        lineOfTokens.add(fieldToken);
-                        field.setLength(0);
-                    }
+					CSVToken stringSeparatorToken = new CSVToken(CSVToken.STRING_SEPARATOR,
+							String.valueOf(format.getStringSeparator()));
+					lineOfTokens.add(stringSeparatorToken);
+					continue;
+				}
 
-                    CSVToken stringSeparatorToken = new CSVToken(CSVToken.STRING_SEPARATOR, String.valueOf(format.getStringSeparator()));
-                    lineOfTokens.add(stringSeparatorToken);
-                    continue;
-                }
+				if (c == format.getFieldSeparator() && !inQuotes) {
+					CSVToken fieldSeparatorToken = new CSVToken(CSVToken.FIELD_SEPARATOR,
+							String.valueOf(format.getFieldSeparator()));
+					lineOfTokens.add(fieldSeparatorToken);
+				} else {
+					field.append(c);
+				}
+			}
 
-                if (c == format.getFieldSeparator() && !inQuotes)
-                {
-                    CSVToken fieldSeparatorToken = new CSVToken(CSVToken.FIELD_SEPARATOR, String.valueOf(format.getFieldSeparator()));
-                    lineOfTokens.add(fieldSeparatorToken);
-                }
-                else
-                {
-                    field.append(c);
-                }
-            }
+			if (!field.toString().trim().isEmpty()) {
+				CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
+				lineOfTokens.add(fieldToken);
+				field.setLength(0);
+			}
 
-            if (!field.toString().trim().isEmpty())
-            {
-                CSVToken fieldToken = new CSVToken(CSVToken.FIELD, field.toString());
-                lineOfTokens.add(fieldToken);
-                field.setLength(0);
-            }
+			if (inQuotes) {
+				CSVToken newLineCSVToken = new CSVToken(CSVToken.NEW_LINE, "\n");
+				lineOfTokens.add(newLineCSVToken);
+			} else {
+				CSVToken lineEndCSVToken = new CSVToken(CSVToken.LINE_END, "");
+				lineOfTokens.add(lineEndCSVToken);
+			}
 
-            if (inQuotes)
-            {
-                CSVToken newLineCSVToken = new CSVToken(CSVToken.NEW_LINE, "\n");
-                lineOfTokens.add(newLineCSVToken);
-            }
-            else
-            {
-                CSVToken lineEndCSVToken = new CSVToken(CSVToken.LINE_END, "");
-                lineOfTokens.add(lineEndCSVToken);
-            }
+			tokens.add(lineOfTokens);
+		}
+	}
 
-            tokens.add(lineOfTokens);
-        }
-    }
-
-    public List<List<CSVToken>> getTokens()
-    {
-        return tokens;
-    }
+	public List<List<CSVToken>> getTokens() {
+		return tokens;
+	}
 }
--- src/main/java/de/uib/configed/csv/CSVToken.java
+++ src/main/java/de/uib/configed/csv/CSVToken.java
@@ -1,47 +1,40 @@
 package de.uib.configed.csv;
 
-public class CSVToken
-{
-    public static final String EMBEDDED_QUOTE = "EMBEDDED_QUOTE";
-    public static final String STRING_SEPARATOR = "STRING_SEPARATOR";
-    public static final String FIELD_SEPARATOR = "FIELD_SEPARATOR";
-    public static final String EMPTY_FIELD = "EMPTY_FIELD";
-    public static final String FIELD = "FIELD";
-    public static final String NEW_LINE = "NEW_LINE";
-    public static final String LINE_END = "LINE_END";
+public class CSVToken {
+	public static final String EMBEDDED_QUOTE = "EMBEDDED_QUOTE";
+	public static final String STRING_SEPARATOR = "STRING_SEPARATOR";
+	public static final String FIELD_SEPARATOR = "FIELD_SEPARATOR";
+	public static final String EMPTY_FIELD = "EMPTY_FIELD";
+	public static final String FIELD = "FIELD";
+	public static final String NEW_LINE = "NEW_LINE";
+	public static final String LINE_END = "LINE_END";
 
-    private String token;
-    private String value;
+	private String token;
+	private String value;
 
-    public CSVToken(String token, String value)
-    {
-        this.token = token;
-        this.value = value;
-    }
+	public CSVToken(String token, String value) {
+		this.token = token;
+		this.value = value;
+	}
 
-    public void setValue(String value)
-    {
-        this.value = value;
-    }
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-    public String getValue()
-    {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public String getName()
-    {
-        return token;
-    }
+	public String getName() {
+		return token;
+	}
 
-    public boolean equals(String token)
-    {
-        return token.equals(this.token);
-    }
+	public boolean equals(String token) {
+		return token.equals(this.token);
+	}
 
-    @Override
-    public String toString()
-    {
-        return String.format("%s: %s", token, value);
-    }
+	@Override
+	public String toString() {
+		return String.format("%s: %s", token, value);
+	}
 }
