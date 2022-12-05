197a50fc4e41d4f365c5fa5f944917e558af8741
Nils Otto <>
2022-12-05 14:19:19 +0100

    Add forgotten changed files

--- src/main/java/de/uib/configed/gui/ssh/SSHWgetAuthenticationPanel.java
+++ src/main/java/de/uib/configed/gui/ssh/SSHWgetAuthenticationPanel.java
@@ -15,9 +15,9 @@ import de.uib.configed.Globals;
 import de.uib.configed.configed;
 
 public class SSHWgetAuthenticationPanel extends SSHPMInstallPanel {
-	static public final String LBLUSER = "lbl_user";
-	static public final String LBLNEEDAUTH = "lbl_needAuthentication";
-	static public final String CBNEEDAUTH = "cb_needAuthentication";
+	public static final String LBLUSER = "lbl_user";
+	public static final String LBLNEEDAUTH = "lbl_needAuthentication";
+	public static final String CBNEEDAUTH = "cb_needAuthentication";
 
 	private JCheckBox cb_needAuthentication;
 	private JLabel lbl_user = new JLabel();
