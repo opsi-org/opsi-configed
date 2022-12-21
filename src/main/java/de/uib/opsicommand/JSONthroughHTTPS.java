42bc8c1efaa82b0fb6aebd5d36ddc7fc3c653758
otto <n.otto@uib.de>
2022-12-21 14:01:37 +0100

    DISABLE_CERTIFICATE_VERIFICATION=true

--- src/main/java/de/uib/opsicommand/JSONthroughHTTPS.java
+++ src/main/java/de/uib/opsicommand/JSONthroughHTTPS.java
@@ -49,7 +49,7 @@ import de.uib.utilities.logging.logging;
  */
 
 public class JSONthroughHTTPS extends JSONthroughHTTP {
-	private static final boolean DISABLE_CERTIFICATE_VERIFICATION = false;
+	private static final boolean DISABLE_CERTIFICATE_VERIFICATION = true;
 
 	public JSONthroughHTTPS(String host, String username, String password) {
 		super(host, username, password);
