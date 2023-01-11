3911b0006260315f141331a6e5227aba7549c8d1
otto <n.otto@uib.de>
2023-01-11 15:31:54 +0100

    change version number after bugfix

--- src/main/java/de/uib/configed/Globals.java
+++ src/main/java/de/uib/configed/Globals.java
@@ -22,8 +22,8 @@ import javafx.stage.Stage;
  */
 
 public class Globals {
-	public static final String VERSION = "4.2.19.7";
-	public static final String VERDATE = "2023-01-05";
+	public static final String VERSION = "4.2.19.8";
+	public static final String VERDATE = "2023-01-11";
 
 	public static final String VERHASHTAG = "";
 
