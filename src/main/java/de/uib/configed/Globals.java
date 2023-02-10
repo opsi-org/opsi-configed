1f9fcff0e58f1963fd9394ac42ad11b90c4f78bd
otto <n.otto@uib.de>
2023-02-10 02:29:23 +0100

    change version to 4.2.20.2 (and version date to today)

--- src/main/java/de/uib/configed/Globals.java
+++ src/main/java/de/uib/configed/Globals.java
@@ -32,8 +32,8 @@ public final class Globals {
 	private Globals() {
 	}
 
-	public static final String VERSION = "4.2.20.1";
-	public static final String VERDATE = "2023-01-23";
+	public static final String VERSION = "4.2.20.2";
+	public static final String VERDATE = "2023-02-09";
 
 	public static final String VERHASHTAG = "";
 
