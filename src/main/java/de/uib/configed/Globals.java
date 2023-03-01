2321abd8f2fd487936bfd1f43b8bdd0d0b6ec9e5
otto <n.otto@uib.de>
2023-03-01 18:18:01 +0100

    update to version 4.2.20.8

--- src/main/java/de/uib/configed/Globals.java
+++ src/main/java/de/uib/configed/Globals.java
@@ -35,8 +35,8 @@ public final class Globals {
 	private Globals() {
 	}
 
-	public static final String VERSION = "4.2.20.7";
-	public static final String VERDATE = "2023-02-28";
+	public static final String VERSION = "4.2.20.8";
+	public static final String VERDATE = "2023-03-01";
 
 	public static final String VERHASHTAG = "";
 
