20ffbdec9322cb7133e3f3e71fa2a05d1e993458
otto <n.otto@uib.de>
2023-03-20 14:29:24 +0100

    make method makeChannelSubscriptionRequest more general, also usable for other subscription requests

--- src/main/java/de/uib/messagebus/Messagebus.java
+++ src/main/java/de/uib/messagebus/Messagebus.java
@@ -7,8 +7,8 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -141,9 +141,7 @@ public class Messagebus {
 		return sslFactory;
 	}
 
-	private void makeChannelSubscriptionRequest(String channel) {
-		List<String> channels = new ArrayList<>();
-		channels.add(String.format("session:%s", channel));
+	private void makeChannelSubscriptionRequest(List<String> channels) {
 
 		Map<String, Object> data = new HashMap<>();
 		data.put("type", "channel_subscription_request");
@@ -169,7 +167,7 @@ public class Messagebus {
 	public void connectTerminal() {
 		String terminalId = UUID.randomUUID().toString();
 
-		makeChannelSubscriptionRequest(terminalId);
+		makeChannelSubscriptionRequest(Collections.singletonList("session:" + terminalId));
 
 		Terminal terminal = Terminal.getInstance();
 		terminal.setMessagebus(this);
