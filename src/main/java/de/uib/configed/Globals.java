f85af55c8a932b2805e63910106511f603b03331
otto <n.otto@uib.de>
2023-04-28 16:23:44 +0200

    new version

--- src/main/java/de/uib/configed/Globals.java
+++ src/main/java/de/uib/configed/Globals.java
@@ -35,7 +35,7 @@ public final class Globals {
 
 	// get version from pom.xml
 	public static final String VERSION = Globals.class.getPackage().getImplementationVersion();
-	public static final String VERDATE = "2023-04-18";
+	public static final String VERDATE = "2023-04-28";
 
 	public static final String VERHASHTAG = "";
 
