9f3aa71c7c8bca25d71d5b85b581af367950bbfd
otto <n.otto@uib.de>
2023-05-02 14:10:08 +0200

    unite two classes (unnecessary extension)

--- src/main/java/de/uib/opsidatamodel/datachanges/HostUpdate.java
+++ src/main/java/de/uib/opsidatamodel/datachanges/HostUpdate.java
@@ -5,14 +5,32 @@ import java.util.Map;
 import de.uib.opsidatamodel.AbstractPersistenceController;
 import de.uib.utilities.logging.Logging;
 
-public class HostUpdate extends AbstractMapUpdate {
+public class HostUpdate implements UpdateCommand {
+
+	String objectId;
+	Map<String, Object> newdata;
+
+	AbstractPersistenceController persis;
+
 	public HostUpdate(AbstractPersistenceController persis, Map<String, Object> newdata) {
-		super(persis, newdata);
+		super();
+		this.newdata = newdata;
+		setController(persis);
 	}
 
 	@Override
 	public void doCall() {
 		Logging.debug(this, "doCall, newdata " + newdata);
 		persis.setHostValues(newdata);
+	}
+
+	@Override
+	public void setController(Object obj) {
+		this.persis = (AbstractPersistenceController) obj;
+	}
+
+	@Override
+	public Object getController() {
+		return persis;
 	}
 }
