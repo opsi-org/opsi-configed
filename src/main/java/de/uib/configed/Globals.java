5a089b7368c8e95f9b044aea532e413c065381c4
otto <n.otto@uib.de>
2023-09-15 12:44:23 +0200

    Update to version 4.3.0.2

--- src/main/java/de/uib/configed/Globals.java
+++ src/main/java/de/uib/configed/Globals.java
@@ -19,7 +19,7 @@ import java.text.DateFormat;
 public final class Globals {
 	// get version from pom.xml
 	public static final String VERSION = Globals.class.getPackage().getImplementationVersion();
-	public static final String VERDATE = "2023-09-12";
+	public static final String VERDATE = "2023-09-15";
 
 	public static final String VERHASHTAG = "";
 
