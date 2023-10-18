opsi-org/opsi-configed

commit 6136e1f7c37d9c9761aac756036fc2460732b414

[ref] Better usage of for loops

src/main/java/de/uib/utilities/table/gui/SensitiveCellEditor.java
@@ -147,9 +147,10 @@ public class SensitiveCellEditor extends AbstractCellEditor implements TableCell
 			} else {
 				StringBuilder buf = new StringBuilder("");
 
-				for (int i = 0; i < list.size() - 1; i++) {
-					buf.append("" + list.get(i) + ",");
+				for (Object element : list) {
+					buf.append("" + element + ",");
 				}
+
 				buf.append("" + list.get(list.size() - 1));
 
 				result = buf.toString();

src/main/java/de/uib/configed/guidata/InstallationStateTableModel.java
@@ -146,10 +146,7 @@ public class InstallationStateTableModel extends AbstractTableModel implements I
 
 		productNamesInDeliveryOrder = new ArrayList<>();
 		if (listOfInstallableProducts != null) {
-			for (int i = 0; i < listOfInstallableProducts.size(); i++) {
-				String product = listOfInstallableProducts.get(i);
-				productNamesInDeliveryOrder.add(product);
-			}
+			productNamesInDeliveryOrder.addAll(listOfInstallableProducts);
 		}
 
 		tsProductNames = new TreeSet<>(myCollator);
@@ -1065,9 +1062,9 @@ public class InstallationStateTableModel extends AbstractTableModel implements I
 
 				// Add in values in correct ordering
 				String[] displayLabels = ActionRequest.getDisplayLabelsForChoice();
-				for (int i = 0; i < displayLabels.length; i += 1) {
-					if (actionList.contains(displayLabels[i])) {
-						actionsForProduct.add(displayLabels[i]);
+				for (String displayLabel : displayLabels) {
+					if (actionList.contains(displayLabel)) {
+						actionsForProduct.add(displayLabel);
 					}
 				}
 

src/main/java/de/uib/utilities/logging/Logging.java
@@ -446,9 +446,9 @@ public final class Logging {
 	public static String getErrorListAsLines() {
 		StringBuilder result = new StringBuilder("");
 		if (!errorList.isEmpty()) {
-			for (int i = 0; i < errorList.size(); i++) {
+			for (String error : errorList) {
 				result.append("\n");
-				result.append(errorList.get(i));
+				result.append(error);
 			}
 		}
 

src/main/java/de/uib/utilities/script/Interpreter.java
@@ -82,8 +82,8 @@ public class Interpreter {
 
 	private void initSpecialValues(String[] specials) {
 		specialValues = new LinkedHashMap<>();
-		for (int i = 0; i < specials.length; i++) {
-			specialValues.put(specials[i], "");
+		for (String special : specials) {
+			specialValues.put(special, "");
 		}
 	}
 

src/main/java/de/uib/utilities/selectionpanel/JTableSelectionPanel.java
@@ -605,9 +605,8 @@ public class JTableSelectionPanel extends JPanel implements DocumentListener, Ke
 	}
 
 	public void fireListSelectionEmpty(Object source) {
-		for (int i = 0; i < selectionmodel.getListSelectionListeners().length; i++) {
-			ListSelectionListener[] listen = selectionmodel.getListSelectionListeners();
-			listen[i].valueChanged(new ListSelectionEvent(source, 0, 0, false));
+		for (ListSelectionListener listener : selectionmodel.getListSelectionListeners()) {
+			listener.valueChanged(new ListSelectionEvent(source, 0, 0, false));
 		}
 	}
 

src/main/java/de/uib/utilities/swing/PopupMenuTrait.java
@@ -52,8 +52,8 @@ public class PopupMenuTrait extends JPopupMenu {
 
 		menuItems = new JMenuItemFormatted[popups.length];
 
-		for (int i = 0; i < popups.length; i++) {
-			addPopup(popups[i]);
+		for (Integer popup : popups) {
+			addPopup(popup);
 		}
 	}
 
@@ -207,8 +207,8 @@ public class PopupMenuTrait extends JPopupMenu {
 	}
 
 	public void addPopupListenersTo(JComponent[] components) {
-		for (int i = 0; i < components.length; i++) {
-			components[i].addMouseListener(new PopupMouseListener(this));
+		for (JComponent component : components) {
+			component.addMouseListener(new PopupMouseListener(this));
 		}
 	}
 

src/main/java/de/uib/utilities/table/GenTableModel.java
@@ -646,21 +646,21 @@ public class GenTableModel extends AbstractTableModel {
 			return;
 		}
 
-		for (int i = 0; i < selection.length; i++) {
-			if (!checkDeletionOfAddedRow(selection[i])) {
+		for (int element : selection) {
+			if (!checkDeletionOfAddedRow(element)) {
 				return;
 			}
 		}
 
 		// do with original row nums
-		for (int i = 0; i < selection.length; i++) {
-			List<Object> oldValues = new ArrayList<>(rows.get(selection[i]));
+		for (int element : selection) {
+			List<Object> oldValues = new ArrayList<>(rows.get(element));
 			Logging.debug(this, "deleteRow values " + oldValues);
 			updates.add(itemFactory.produceDeleteItem(oldValues));
 
-			if (updatedRows.contains(selection[i])) {
-				Logging.debug(this, "deleteRows, remove from updatedRows  " + selection[i]);
-				updatedRows.remove(selection[i]);
+			if (updatedRows.contains(element)) {
+				Logging.debug(this, "deleteRows, remove from updatedRows  " + element);
+				updatedRows.remove(element);
 			}
 		}
 

src/main/java/de/uib/utilities/table/gui/SearchTargetModelFromTable.java
@@ -167,8 +167,8 @@ public class SearchTargetModelFromTable implements SearchTargetModel {
 	public void setSelection(int[] selection) {
 		Logging.info(this, "setSelection --- " + Arrays.toString(selection));
 		table.getSelectionModel().clearSelection();
-		for (int i = 0; i < selection.length; i++) {
-			table.getSelectionModel().addSelectionInterval(selection[i], selection[i]);
+		for (int selectionElement : selection) {
+			table.getSelectionModel().addSelectionInterval(selectionElement, selectionElement);
 		}
 	}
 

commit bf46687fd2a3618ade9f6e0e1c375851acb4a955

[fix] Double values added when editing properties
This commit fixes issue of double values added when editing a property,
that can have multiple values. This issue arises due to incorrect
implementation of a for loop, that handles addition of multiple values
(this bug was introduced in commit 6136e1f7c37d9c9761aac756036fc2460732b414).

In the commit 6136e1f7c37d9c9761aac756036fc2460732b414 it was done so
that the last element would be added twice.

To fix this issue we revert to previous implementation prior to commit
6136e1f7c37d9c9761aac756036fc2460732b414.

src/main/java/de/uib/utilities/table/gui/SensitiveCellEditor.java
@@ -147,8 +147,8 @@ public class SensitiveCellEditor extends AbstractCellEditor implements TableCell
 			} else {
 				StringBuilder buf = new StringBuilder("");
 
-				for (Object element : list) {
-					buf.append("" + element + ",");
+				for (int i = 0; i < list.size() - 1; i++) {
+					buf.append("" + list.get(i) + ",");
 				}
 
 				buf.append("" + list.get(list.size() - 1));