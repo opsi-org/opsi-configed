opsi-org/opsi-configed

commit 18854cb0b6f35f4bc176f0f196175d25dbdc9bd4

Fix Linter Warnings: java:S2447, java:S5411, java:S1168

src/main/java/de/uib/utilities/table/gui/BooleanIconTableCellRenderer.java
@@ -59,33 +59,19 @@ public class BooleanIconTableCellRenderer extends StandardTableCellRenderer {
 		label.setIcon(null);
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 
-		Boolean val = null;
-
-		if (value != null) {
-			if (value instanceof Boolean)
-				val = (Boolean) value;
-
-			else // (value instanceof String)
-			{
-				if (((String) value).equalsIgnoreCase("true"))
-					val = true;
-				else if (((String) value).equalsIgnoreCase("false"))
-					val = false;
-			}
-		}
-
-		if (val == null) {
-
-			if (nullIcon != null)
+		if (value == null) {
+			if (nullIcon != null) {
 				label.setIcon(nullIcon);
+			}
 		} else {
-
-			if (val) {
-				if (trueIcon != null)
+			if (Boolean.TRUE.equals(value)) {
+				if (trueIcon != null) {
 					label.setIcon(trueIcon);
+				}
 			} else {
-				if (falseIcon != null)
+				if (falseIcon != null) {
 					label.setIcon(falseIcon);
+				}
 			}
 		}
 

src/main/java/de/uib/configed/ConfigedMain.java
@@ -1231,7 +1231,7 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 
 		// String[] oldSelectedDepots =
 
-		if (oldSelectedDepots == null) {
+		if (oldSelectedDepots.length == 0) {
 			depotsList.setSelectedValue(myServer, true);
 		} else {
 			ArrayList<Integer> savedSelectedDepots = new ArrayList<>();
@@ -1973,7 +1973,7 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 
 		final int iconColumnMaxWidth = 100;
 
-		if (persist.getHostDisplayFields().get(HostInfo.CLIENT_CONNECTED_DISPLAY_FIELD_LABEL)) {
+		if (Boolean.TRUE.equals(persist.getHostDisplayFields().get(HostInfo.CLIENT_CONNECTED_DISPLAY_FIELD_LABEL))) {
 			int col = selectionPanel.getTableModel().findColumn(Configed.getResourceValue(
 					"ConfigedMain.pclistTableModel." + HostInfo.CLIENT_CONNECTED_DISPLAY_FIELD_LABEL));
 
@@ -1988,7 +1988,7 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 
 		}
 
-		if (persist.getHostDisplayFields().get(HostInfo.CLIENT_UEFI_BOOT_DISPLAY_FIELD_LABEL)) {
+		if (Boolean.TRUE.equals(persist.getHostDisplayFields().get(HostInfo.CLIENT_UEFI_BOOT_DISPLAY_FIELD_LABEL))) {
 
 			List<String> columns = new ArrayList<>();
 			for (int i = 0; i < selectionPanel.getTableModel().getColumnCount(); i++) {
@@ -2019,7 +2019,7 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 			}
 		}
 
-		if (persist.getHostDisplayFields().get(HostInfo.CLIENT_WAN_CONFIG_DISPLAY_FIELD_LABEL)) {
+		if (Boolean.TRUE.equals(persist.getHostDisplayFields().get(HostInfo.CLIENT_WAN_CONFIG_DISPLAY_FIELD_LABEL))) {
 
 			List<String> columns = new ArrayList<>();
 			for (int i = 0; i < selectionPanel.getTableModel().getColumnCount(); i++) {
@@ -2048,7 +2048,8 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 
 		}
 
-		if (persist.getHostDisplayFields().get(HostInfo.CLIENT_INSTALL_BY_SHUTDOWN_DISPLAY_FIELD_LABEL)) {
+		if (Boolean.TRUE
+				.equals(persist.getHostDisplayFields().get(HostInfo.CLIENT_INSTALL_BY_SHUTDOWN_DISPLAY_FIELD_LABEL))) {
 
 			List<String> columns = new ArrayList<>();
 
@@ -4125,7 +4126,7 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 						// we catch exceptions especially if we are on some updating process for the
 						// model
 
-						if (persist.getHostDisplayFields().get("clientConnected")) {
+						if (Boolean.TRUE.equals(persist.getHostDisplayFields().get("clientConnected"))) {
 							Map<String, Object> saveReachableInfo = reachableInfo;
 
 							reachableInfo = persist.reachableInfo(null);
@@ -4230,7 +4231,7 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 				mainFrame.iconButtonReachableInfo.setEnabled(true);
 
 				// update column
-				if (persist.getHostDisplayFields().get("clientConnected")) {
+				if (Boolean.TRUE.equals(persist.getHostDisplayFields().get("clientConnected"))) {
 					javax.swing.table.AbstractTableModel model = selectionPanel.getTableModel();
 
 					int col = model
@@ -4312,7 +4313,7 @@ public class ConfigedMain implements ListSelectionListener, TabController, LogEv
 								mainFrame.iconButtonSessionInfo.setEnabled(true);
 
 								// update column
-								if (persist.getHostDisplayFields().get("clientSessionInfo")) {
+								if (Boolean.TRUE.equals(persist.getHostDisplayFields().get("clientSessionInfo"))) {
 									javax.swing.table.AbstractTableModel model = selectionPanel.getTableModel();
 
 									int col = model.findColumn(Configed

src/main/java/de/uib/configed/Globals.java
@@ -97,18 +97,16 @@ public class Globals {
 	public static final int DEFAULT_FTEXTAREA_HEIGHT = 200;
 	public static final int DEFAULT_FTEXTAREA_WIDTH = 350;
 
-	public static Boolean interpretAsBoolean(Object value) {
+	public static boolean interpretAsBoolean(Object value) {
 
 		if (value == null)
-			return null;
+			return false;
 
 		if (value instanceof Boolean) {
-
 			return (Boolean) value;
 		}
 
 		if (value instanceof Integer) {
-
 			int val = (Integer) value;
 			if (val == 1)
 				return true;
@@ -124,7 +122,7 @@ public class Globals {
 			String val = ((String) value).toLowerCase();
 
 			if (val.equals(""))
-				return null;
+				return false;
 
 			if (val.equals("true"))
 				return true;

src/main/java/de/uib/configed/clientselection/BackendMySQL.java
@@ -334,7 +334,6 @@ public class BackendMySQL {
 	}
 
 	public List<String> getClientListFromJSONString(String query) {
-
 		Logging.info(this, query);
 
 		try {
@@ -342,12 +341,11 @@ public class BackendMySQL {
 
 			if (jsonObject.has("data"))
 				return getListFromJSONObject(jsonObject.getJSONObject("data"));
-
 		} catch (JSONException e) {
 			Logging.warning(this, "" + e);
 		}
 
-		return null;
+		return new ArrayList<>();
 	}
 
 	private String doJSONObject(MySQL mySQLRecursion, JSONObject jsonObject) {

src/main/java/de/uib/configed/clientselection/SavedSearchQuery.java
@@ -1,5 +1,6 @@
 package de.uib.configed.clientselection;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -90,7 +91,7 @@ public class SavedSearchQuery {
 	}
 
 	public List<String> runSearch(boolean printing) {
-		List<String> result = null;
+		List<String> result = new ArrayList<>();
 
 		Messages.setLocale("en");
 		controller = PersistenceControllerFactory.getNewPersistenceController(host, user, password);
@@ -114,7 +115,7 @@ public class SavedSearchQuery {
 		List<String> searches = manager.getSavedSearchesNames();
 		if (searchName == null && printing) {
 			printResult(searches);
-			return null;
+			return new ArrayList<>();
 		}
 
 		if (!searches.contains(searchName)) {

src/main/java/de/uib/configed/clientselection/SelectionManager.java
@@ -187,7 +187,7 @@ public class SelectionManager {
 		SelectOperation operation = getTopOperation();
 		if (operation == null) {
 			Logging.info(this, "Nothing selected");
-			return null;
+			return new ArrayList<>();
 		} else {
 			Logging.info("\n" + operation.printOperation(""));
 		}

src/main/java/de/uib/configed/gui/MainFrame.java
@@ -3288,9 +3288,9 @@ public class MainFrame extends JFrame
 
 		else if (e.getSource() == jButtonLicences || e.getSource() == jMenuFrameLicences) {
 			configedMain.handleLicencesManagementRequest();
-			if (configedMain.getPersistenceController().getGlobalBooleanConfigValue(
+			if (Boolean.TRUE.equals(configedMain.getPersistenceController().getGlobalBooleanConfigValue(
 					PersistenceController.KEY_SHOW_DASH_FOR_LICENCEMANAGEMENT,
-					PersistenceController.DEFAULTVALUE_SHOW_DASH_FOR_LICENCEMANAGEMENT)) {
+					PersistenceController.DEFAULTVALUE_SHOW_DASH_FOR_LICENCEMANAGEMENT))) {
 
 				if (licenseDash == null) {
 					licenseDash = new LicenseDash();

src/main/java/de/uib/configed/gui/SavedSearchesDialog.java
@@ -157,7 +157,7 @@ public class SavedSearchesDialog extends FEditList {
 
 	@Override
 	protected void commit() {
-		result = null;
+		result = new LinkedList<>();
 
 		buttonCommit.setEnabled(false);
 		buttonCancel.setEnabled(false);
@@ -184,7 +184,7 @@ public class SavedSearchesDialog extends FEditList {
 
 	@Override
 	protected void cancel() {
-		result = null;
+		result = new LinkedList<>();
 
 		super.cancel();
 	}

commit 994cc8099e0813c15cb895f1b158ad9e537a6d8c

[fix] Fix string conversion to boolean
This commit fixes issue of improper conversion from String to Boolean.
The issue was introduced in commit
18854cb0b6f35f4bc176f0f196175d25dbdc9bd4.

src/main/java/de/uib/utilities/table/gui/BooleanIconTableCellRenderer.java
@@ -56,13 +56,27 @@ public class BooleanIconTableCellRenderer extends StandardTableCellRenderer {
 		label.setText("");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 
-		if (Boolean.TRUE.equals(value)) {
+		if (objAsBoolean(value).booleanValue()) {
 			label.setIcon(trueIcon);
 		} else {
 			label.setIcon(falseIcon);
 		}
 
 		return comp;
+	}
+
+	private Boolean objAsBoolean(Object obj) {
+		Boolean result = Boolean.FALSE;
+		if (obj != null) {
+			if (obj instanceof Boolean) {
+				result = (Boolean) obj;
+			} else if (obj instanceof String) {
+				result = Boolean.valueOf((String) obj);
+			} else {
+				Logging.warning(this, "Unexpected obj type " + obj);
+			}
+		}
+		return result;
 	}
 
 }