f9aa219d9b740934ad31f0e9416da0d03011aa97
otto <n.otto@uib.de>
2023-10-24 11:23:14 +0200

    [fix] Show complete text in buttons in FTextArea

--- src/main/java/de/uib/configed/gui/FTextArea.java
+++ src/main/java/de/uib/configed/gui/FTextArea.java
@@ -12,14 +12,15 @@ import java.awt.event.KeyEvent;
 import javax.swing.Icon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
 import javax.swing.text.JTextComponent;
 
 import de.uib.configed.Globals;
 
 public class FTextArea extends FGeneralDialog {
 
-	private JTextArea jTextArea1 = new JTextArea();
+	private JTextPane jTextPane = new JTextPane();
 
 	public FTextArea(JFrame owner, String title, boolean modal, String[] buttonList) {
 		super(owner, title, modal, buttonList);
@@ -56,12 +57,12 @@ public class FTextArea extends FGeneralDialog {
 	}
 
 	public void setMessage(String message) {
-		jTextArea1.setText(message);
-		jTextArea1.setCaretPosition(0);
+		jTextPane.setText(message);
+		jTextPane.setCaretPosition(0);
 	}
 
 	public JTextComponent getTextComponent() {
-		return jTextArea1;
+		return jTextPane;
 	}
 
 	private void initFTextArea() {
@@ -75,15 +76,14 @@ public class FTextArea extends FGeneralDialog {
 
 	private void initFTextArea(int preferredWidth, int preferredHeight) {
 		allpane.setPreferredSize(new Dimension(preferredWidth, preferredHeight));
-		jTextArea1.setLineWrap(true);
-		jTextArea1.setWrapStyleWord(true);
 
-		jTextArea1.setText("          ");
-		jTextArea1.setEditable(false);
+		jTextPane.setAlignmentX(SwingConstants.CENTER);
+		jTextPane.setText("          ");
+		jTextPane.setEditable(false);
 
-		scrollpane.getViewport().add(jTextArea1, null);
+		scrollpane.getViewport().add(jTextPane, null);
 
-		jTextArea1.addKeyListener(this);
+		jTextPane.addKeyListener(this);
 
 		pack();
 	}
@@ -96,12 +96,12 @@ public class FTextArea extends FGeneralDialog {
 			shiftPressed = false;
 		}
 
-		if (!shiftPressed && e.getSource() == jTextArea1 && e.getKeyCode() == KeyEvent.VK_TAB) {
+		if (!shiftPressed && e.getSource() == jTextPane && e.getKeyCode() == KeyEvent.VK_TAB) {
 			jButton1.requestFocus();
 		}
 
 		if (shiftPressed && e.getSource() == jButton1 && e.getKeyCode() == KeyEvent.VK_TAB) {
-			jTextArea1.requestFocus();
+			jTextPane.requestFocus();
 		}
 	}
 }
