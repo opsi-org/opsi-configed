c7b81f3140cea23a1e61528564e2c45d1ea39fb0
vidziunas <n.vidziunas@uib.de>
2023-12-05 17:30:41 +0100

    [ref] Handling of dark/light color palette

---
+++ src/main/java/de/uib/configed/terminal/ColorPaletteImpl.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) uib GmbH <info@uib.de>
+ * License: AGPL-3.0
+ * This file is part of opsi - https://www.opsi.org
+ */
+
+package de.uib.configed.terminal;
+
+import com.jediterm.core.Color;
+import com.jediterm.terminal.emulator.ColorPalette;
+
+public class ColorPaletteImpl extends ColorPalette {
+	private static final Color[] DEFAULT_COLORS = new Color[16];
+	static {
+		// Black
+		DEFAULT_COLORS[0] = new Color(0x000000);
+		// Red
+		DEFAULT_COLORS[1] = new Color(0xff0000);
+		// Green
+		DEFAULT_COLORS[2] = new Color(0x33ff00);
+		// Yellow
+		DEFAULT_COLORS[3] = new Color(0xf3e207);
+		// Blue
+		DEFAULT_COLORS[4] = new Color(0x0066ff);
+		// Magenta
+		DEFAULT_COLORS[5] = new Color(0xe197f4);
+		// Cyan
+		DEFAULT_COLORS[6] = new Color(0x00ffff);
+		// White
+		DEFAULT_COLORS[7] = new Color(0xd0d0d0);
+
+		// Bright versions of the ISO colors
+
+		// Black
+		DEFAULT_COLORS[8] = new Color(0x808080);
+		// Red
+		DEFAULT_COLORS[9] = new Color(0xff0000);
+		// Green
+		DEFAULT_COLORS[10] = new Color(0x33ff00);
+		// Yellow
+		DEFAULT_COLORS[11] = new Color(0xf3e207);
+		// Blue
+		DEFAULT_COLORS[12] = new Color(0x0066ff);
+		// Magenta
+		DEFAULT_COLORS[13] = new Color(0xe197f4);
+		// Cyan
+		DEFAULT_COLORS[14] = new Color(0x00ffff);
+		// White
+		DEFAULT_COLORS[15] = new Color(0xffffff);
+	}
+
+	public static final ColorPalette DEFAULT_COLOR_PALETTE = new ColorPaletteImpl(DEFAULT_COLORS);
+
+	private Color[] colors;
+
+	public ColorPaletteImpl(Color[] colors) {
+		this.colors = colors;
+	}
+
+	@Override
+	public Color getForegroundByColorIndex(int colorIndex) {
+		return colors[colorIndex];
+	}
+
+	@Override
+	public Color getBackgroundByColorIndex(int colorIndex) {
+		return colors[colorIndex];
+	}
+
+}
--- src/main/java/de/uib/configed/terminal/TerminalSettingsProvider.java
+++ src/main/java/de/uib/configed/terminal/TerminalSettingsProvider.java
@@ -8,97 +8,23 @@ package de.uib.configed.terminal;
 
 import java.awt.Font;
 
-import com.jediterm.core.Color;
+import com.jediterm.terminal.TerminalColor;
+import com.jediterm.terminal.TextStyle;
 import com.jediterm.terminal.emulator.ColorPalette;
 import com.jediterm.terminal.ui.settings.DefaultSettingsProvider;
 
 import de.uib.messages.Messages;
-import de.uib.utilities.logging.Logging;
 
 public class TerminalSettingsProvider extends DefaultSettingsProvider {
 	public static final int FONT_SIZE_MIN_LIMIT = 8;
 	public static final int FONT_SIZE_MAX_LIMIT = 62;
+	public static final TerminalColor BACKGROUND_COLOR_DARK = new TerminalColor(0, 0, 0);
+	public static final TerminalColor FOREGROUND_COLOR_DARK = new TerminalColor(208, 208, 208);
+	public static final TerminalColor BACKGROUND_COLOR_LIGHT = new TerminalColor(249, 249, 249);
+	public static final TerminalColor FOREGROUND_COLOR_LIGHT = new TerminalColor(96, 96, 96);
 
 	private static int fontSize = 12;
-	private static final Color[] DARK_COLORS = new Color[16];
-	static {
-		// Black
-		DARK_COLORS[0] = new Color(255, 255, 255);
-		// Red
-		DARK_COLORS[1] = new Color(255, 0, 0);
-		// Green
-		DARK_COLORS[2] = new Color(51, 255, 0);
-		// Yellow
-		DARK_COLORS[3] = new Color(255, 0, 153);
-		// Blue
-		DARK_COLORS[4] = new Color(0, 102, 255);
-		// Magenta
-		DARK_COLORS[5] = new Color(204, 0, 255);
-		// Cyan
-		DARK_COLORS[6] = new Color(0, 255, 255);
-		// White
-		DARK_COLORS[7] = new Color(208, 208, 208);
-
-		// Bright versions of the ISO colors
-
-		// Black
-		DARK_COLORS[8] = new Color(128, 128, 128);
-		// Red
-		DARK_COLORS[9] = new Color(255, 0, 0);
-		// Green
-		DARK_COLORS[10] = new Color(51, 255, 0);
-		// Yellow
-		DARK_COLORS[11] = new Color(255, 0, 153);
-		// Blue
-		DARK_COLORS[12] = new Color(0, 102, 255);
-		// Magenta
-		DARK_COLORS[13] = new Color(204, 0, 255);
-		// Cyan
-		DARK_COLORS[14] = new Color(0, 255, 255);
-		// White
-		DARK_COLORS[15] = new Color(0, 0, 0);
-	}
-	private static final Color[] LIGHT_COLORS = new Color[16];
-	static {
-		// Black
-		LIGHT_COLORS[0] = new Color(0, 0, 0);
-		// Red
-		LIGHT_COLORS[1] = new Color(255, 0, 0);
-		// Green
-		LIGHT_COLORS[2] = new Color(51, 255, 0);
-		// Yellow
-		LIGHT_COLORS[3] = new Color(255, 0, 153);
-		// Blue
-		LIGHT_COLORS[4] = new Color(0, 102, 255);
-		// Magenta
-		LIGHT_COLORS[5] = new Color(204, 0, 255);
-		// Cyan
-		LIGHT_COLORS[6] = new Color(0, 255, 255);
-		// White
-		LIGHT_COLORS[7] = new Color(208, 208, 208);
-
-		// Bright versions of the ISO colors
-
-		// Black
-		LIGHT_COLORS[8] = new Color(128, 128, 128);
-		// Red
-		LIGHT_COLORS[9] = new Color(255, 0, 0);
-		// Green
-		LIGHT_COLORS[10] = new Color(51, 255, 0);
-		// Yellow
-		LIGHT_COLORS[11] = new Color(255, 0, 153);
-		// Blue
-		LIGHT_COLORS[12] = new Color(0, 102, 255);
-		// Magenta
-		LIGHT_COLORS[13] = new Color(204, 0, 255);
-		// Cyan
-		LIGHT_COLORS[14] = new Color(0, 255, 255);
-		// White
-		LIGHT_COLORS[15] = new Color(255, 255, 255);
-	}
-
 	private static String themeInUse = Messages.getSelectedTheme();
-	private static MyColorPalette colorPalette = getColorPalette(themeInUse);
 
 	@Override
 	public Font getTerminalFont() {
@@ -116,8 +42,17 @@ public class TerminalSettingsProvider extends DefaultSettingsProvider {
 	}
 
 	@Override
+	public TextStyle getDefaultStyle() {
+		TextStyle defaultStyle = new TextStyle(FOREGROUND_COLOR_DARK, BACKGROUND_COLOR_DARK);
+		if ("Light".equals(themeInUse)) {
+			defaultStyle = new TextStyle(FOREGROUND_COLOR_LIGHT, BACKGROUND_COLOR_LIGHT);
+		}
+		return defaultStyle;
+	}
+
+	@Override
 	public ColorPalette getTerminalColorPalette() {
-		return colorPalette;
+		return ColorPaletteImpl.DEFAULT_COLOR_PALETTE;
 	}
 
 	@Override
@@ -139,44 +74,11 @@ public class TerminalSettingsProvider extends DefaultSettingsProvider {
 		fontSize = size;
 	}
 
-	private static MyColorPalette getColorPalette(String theme) {
-		switch (theme) {
-		case Messages.THEME_LIGHT:
-			return new MyColorPalette(LIGHT_COLORS);
-
-		case Messages.THEME_DARK:
-			return new MyColorPalette(DARK_COLORS);
-
-		default:
-			Logging.warning("Could not find MyColorPalette for theme " + theme);
-			return new MyColorPalette(DARK_COLORS);
-		}
-	}
-
 	public static void setTerminalTheme(String theme) {
 		themeInUse = theme;
-		colorPalette = getColorPalette(themeInUse);
 	}
 
 	public static String getTerminalThemeInUse() {
 		return themeInUse;
-	}
-
-	private static class MyColorPalette extends ColorPalette {
-		private Color[] colors;
-
-		public MyColorPalette(Color[] colors) {
-			this.colors = colors;
-		}
-
-		@Override
-		public Color getForegroundByColorIndex(int colorIndex) {
-			return colors[colorIndex];
-		}
-
-		@Override
-		public Color getBackgroundByColorIndex(int colorIndex) {
-			return colors[colorIndex];
-		}
 	}
 }
