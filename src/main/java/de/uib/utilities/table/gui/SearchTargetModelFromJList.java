b16574e299b82935478a127be1918692f569c03b
otto <n.otto@uib.de>
2023-12-28 19:05:37 +0100

    [ref] Remove unnecessary varible

--- src/main/java/de/uib/utilities/table/gui/SearchTargetModelFromJList.java
+++ src/main/java/de/uib/utilities/table/gui/SearchTargetModelFromJList.java
@@ -27,8 +27,6 @@ public class SearchTargetModelFromJList extends SearchTargetModelFromTable {
 	private List<String> unfilteredD;
 	private int[] unfilteredSelection;
 
-	private boolean filtered;
-
 	public SearchTargetModelFromJList(JList<String> jList, final List<String> values, final List<String> descriptions) {
 		this.jList = jList;
 		unfilteredV = values;
