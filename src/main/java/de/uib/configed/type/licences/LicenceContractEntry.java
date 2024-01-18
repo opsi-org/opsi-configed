2c3afc1e527a39f27b6d07ea80fcfb6af1555c57
vidziunas <n.vidziunas@uib.de>
2024-01-18 14:42:29 +0100

    [rem] `TableLicenceContracts` class

--- src/main/java/de/uib/configed/type/licences/LicenceContractEntry.java
+++ src/main/java/de/uib/configed/type/licences/LicenceContractEntry.java
@@ -6,14 +6,59 @@
 
 package de.uib.configed.type.licences;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import de.uib.utilities.datastructure.StringValuedRelationElement;
 
 public class LicenceContractEntry extends StringValuedRelationElement {
+	public static final String ID_KEY = "id";
+	public static final String IDENT_KEY = "ident";
+	public static final String ID_DB_KEY = "licenseContractId";
+	public static final String PARTNER_KEY = "partner";
+	public static final String CONCLUSION_DATE_KEY = "conclusionDate";
+	public static final String NOTIFICATION_DATE_KEY = "notificationDate";
+	public static final String EXPIRATION_DATE_KEY = "expirationDate";
+	public static final String NOTES_KEY = "notes";
+	public static final String DESCRIPTION_KEY = "description";
+
+	public static final String TYPE_KEY = "type";
+
+	private static final List<String> DB_ATTRIBUTES;
+	static {
+		DB_ATTRIBUTES = new LinkedList<>();
+		DB_ATTRIBUTES.add(ID_DB_KEY);
+		DB_ATTRIBUTES.add(PARTNER_KEY);
+		DB_ATTRIBUTES.add(CONCLUSION_DATE_KEY);
+		DB_ATTRIBUTES.add(NOTIFICATION_DATE_KEY);
+		DB_ATTRIBUTES.add(EXPIRATION_DATE_KEY);
+		DB_ATTRIBUTES.add(NOTES_KEY);
+		DB_ATTRIBUTES.add(DESCRIPTION_KEY);
+	}
+
+	private static final List<String> INTERFACED_ATTRIBUTES;
+	static {
+		INTERFACED_ATTRIBUTES = new LinkedList<>();
+		INTERFACED_ATTRIBUTES.add(ID_DB_KEY);
+		INTERFACED_ATTRIBUTES.add(PARTNER_KEY);
+		INTERFACED_ATTRIBUTES.add(CONCLUSION_DATE_KEY);
+		INTERFACED_ATTRIBUTES.add(NOTIFICATION_DATE_KEY);
+		INTERFACED_ATTRIBUTES.add(EXPIRATION_DATE_KEY);
+		INTERFACED_ATTRIBUTES.add(NOTES_KEY);
+	}
+
+	private static final List<String> ALLOWED_ATTRIBUTES;
+	static {
+		ALLOWED_ATTRIBUTES = new LinkedList<>(DB_ATTRIBUTES);
+		ALLOWED_ATTRIBUTES.add(ID_KEY);
+		ALLOWED_ATTRIBUTES.add(IDENT_KEY);
+		ALLOWED_ATTRIBUTES.add(TYPE_KEY);
+	}
+
 	public LicenceContractEntry() {
 		super();
-		super.setAllowedAttributes(TableLicenceContracts.ALLOWED_ATTRIBUTES);
+		super.setAllowedAttributes(ALLOWED_ATTRIBUTES);
 	}
 
 	public LicenceContractEntry(Map<String, Object> m) {
@@ -36,17 +81,17 @@ public class LicenceContractEntry extends StringValuedRelationElement {
 	protected final void produceFrom(Map<String, ? extends Object> map) {
 		super.produceFrom(map);
 
-		put(TableLicenceContracts.ID_DB_KEY, get(TableLicenceContracts.ID_KEY));
-		remove(TableLicenceContracts.TYPE_KEY);
-		remove(TableLicenceContracts.IDENT_KEY);
+		put(ID_DB_KEY, get(ID_KEY));
+		remove(TYPE_KEY);
+		remove(IDENT_KEY);
 
-		put(TableLicenceContracts.CONCLUSION_DATE_KEY, removeTime(get(TableLicenceContracts.CONCLUSION_DATE_KEY)));
+		put(CONCLUSION_DATE_KEY, removeTime(get(CONCLUSION_DATE_KEY)));
 
-		put(TableLicenceContracts.NOTIFICATION_DATE_KEY, removeTime(get(TableLicenceContracts.NOTIFICATION_DATE_KEY)));
-		put(TableLicenceContracts.EXPIRATION_DATE_KEY, removeTime(get(TableLicenceContracts.EXPIRATION_DATE_KEY)));
+		put(NOTIFICATION_DATE_KEY, removeTime(get(NOTIFICATION_DATE_KEY)));
+		put(EXPIRATION_DATE_KEY, removeTime(get(EXPIRATION_DATE_KEY)));
 	}
 
 	public String getId() {
-		return get(TableLicenceContracts.ID_DB_KEY);
+		return get(ID_DB_KEY);
 	}
 }
