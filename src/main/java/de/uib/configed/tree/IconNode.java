836826a87b386fb96e4deead4da50182aa286117
otto <n.otto@uib.de>
2024-01-23 17:59:41 +0100

    [ref] Remove unnecessary code

--- src/main/java/de/uib/configed/tree/IconNode.java
+++ src/main/java/de/uib/configed/tree/IconNode.java
@@ -6,26 +6,17 @@
 
 package de.uib.configed.tree;
 
-import java.awt.Image;
 import java.util.Map;
 
-import javax.swing.GrayFilter;
 import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class IconNode extends DefaultMutableTreeNode {
 	private Icon closedIcon;
-	private Icon disabledClosedIcon;
-	private Icon enabledClosedIcon;
 
 	private Icon leafIcon;
-	private Icon disabledLeafIcon;
-	private Icon enabledLeafIcon;
 
 	private Icon openIcon;
-	private Icon disabledOpenIcon;
-	private Icon enabledOpenIcon;
 
 	private String toolTipText;
 
@@ -56,44 +47,6 @@ public class IconNode extends DefaultMutableTreeNode {
 		return deviceInfo;
 	}
 
-	public void setEnabled(boolean aFlag) {
-		if (!aFlag) {
-			// Lazy creation: avoids unneccessary objects if the tree
-			// could not have disabled state.
-			if (closedIcon != null && disabledClosedIcon == null) {
-				disabledClosedIcon = createDisabledIcon(enabledClosedIcon);
-			}
-
-			setDisabledLeafIcon();
-
-			if (openIcon != null && disabledOpenIcon == null) {
-				disabledOpenIcon = createDisabledIcon(enabledOpenIcon);
-			}
-			// end of lazy creation
-
-			closedIcon = disabledClosedIcon;
-			leafIcon = disabledLeafIcon;
-			openIcon = disabledOpenIcon;
-		} else {
-			closedIcon = enabledClosedIcon;
-			leafIcon = enabledLeafIcon;
-			openIcon = enabledOpenIcon;
-		}
-	}
-
-	/**
-	 * Try to create grayed icon from aIcon and return it, or return null.
-	 */
-	private static Icon createDisabledIcon(Icon anIcon) {
-		// copied from your example: e601. Creating a Gray Version of an Icon
-		if (anIcon instanceof ImageIcon) {
-			Image grayImage = GrayFilter.createDisabledImage(((ImageIcon) anIcon).getImage());
-			return new ImageIcon(grayImage);
-		}
-		// Cannot convert
-		return null;
-	}
-
 	public Icon getClosedIcon() {
 		return closedIcon;
 	}
@@ -103,12 +56,10 @@ public class IconNode extends DefaultMutableTreeNode {
 		setClosedIcon(anIcon);
 		setLeafIcon(anIcon);
 		setOpenIcon(anIcon);
-		setDisabledLeafIcon();
 	}
 
 	public void setClosedIcon(Icon aClosedIcon) {
 		closedIcon = aClosedIcon;
-		enabledClosedIcon = aClosedIcon;
 	}
 
 	public Icon getLeafIcon() {
@@ -117,7 +68,6 @@ public class IconNode extends DefaultMutableTreeNode {
 
 	public void setLeafIcon(Icon aLeafIcon) {
 		leafIcon = aLeafIcon;
-		enabledLeafIcon = aLeafIcon;
 	}
 
 	public Icon getOpenIcon() {
@@ -126,12 +76,5 @@ public class IconNode extends DefaultMutableTreeNode {
 
 	public void setOpenIcon(Icon anOpenIcon) {
 		openIcon = anOpenIcon;
-		enabledOpenIcon = anOpenIcon;
-	}
-
-	public void setDisabledLeafIcon() {
-		if (leafIcon != null) {
-			disabledLeafIcon = createDisabledIcon(leafIcon);
-		}
 	}
 }
