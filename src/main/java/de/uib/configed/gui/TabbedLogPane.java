a2f654d98d4d0bd7e0fcfa0734e960b5b2380731
Naglis Vidziunas <n.vidziunas@uib.de>
2024-01-30 11:06:54 +0100

    [ref] Extract anonymous class from `TabbedLogPane`

--- src/main/java/de/uib/configed/gui/TabbedLogPane.java
+++ src/main/java/de/uib/configed/gui/TabbedLogPane.java
@@ -11,47 +11,30 @@ import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import javax.swing.Icon;
-import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.plaf.synth.Region;
 import javax.swing.plaf.synth.SynthConstants;
 import javax.swing.plaf.synth.SynthContext;
 import javax.swing.plaf.synth.SynthLookAndFeel;
 import javax.swing.plaf.synth.SynthStyle;
 
-import de.uib.configed.Configed;
 import de.uib.configed.ConfigedMain;
 import de.uib.configed.gui.logpane.LogPane;
-import de.uib.opsidatamodel.serverdata.PersistenceControllerFactory;
 import de.uib.utilities.logging.Logging;
 
 public class TabbedLogPane extends JTabbedPane {
-	private static final String ALL_LOGFILES_SUFFIX = "all";
-	private static final byte[] CRLF = new byte[] { '\r', '\n' };
-
 	private LogPane[] textPanes;
 	private String[] idents;
 	private final List<String> identsList;
 
-	private JFileChooser chooser;
-	private File chooserDirectory;
 	private ConfigedMain configedMain;
 
 	public TabbedLogPane(final String[] idents, String defaultText, ConfigedMain configedMain) {
@@ -63,7 +46,7 @@ public class TabbedLogPane extends JTabbedPane {
 		textPanes = new LogPane[idents.length];
 
 		for (int i = 0; i < idents.length; i++) {
-			initLogFrame(i, defaultText);
+			initLogTabComponent(i, defaultText);
 		}
 
 		super.addComponentListener(new ComponentAdapter() {
@@ -146,171 +129,17 @@ public class TabbedLogPane extends JTabbedPane {
 		initTabWidth();
 	}
 
-	private void initLogFrame(int i, String defaultText) {
-		LogPane showPane = new LogPane(defaultText, true) {
-			@Override
-			public void reload() {
-				super.reload();
-				ConfigedMain.getMainFrame().activateLoadingCursor();
-				loadDocument(idents[i]);
-				ConfigedMain.getMainFrame().deactivateLoadingCursor();
-			}
-
-			@Override
-			public void save() {
-				ConfigedMain.getMainFrame().activateLoadingCursor();
-				String fileName = retrieveFileName(getInfo(), idents[i]);
-				String filePath = retrieveFilePath(fileName + ".log");
-				if (filePath != null && !filePath.isEmpty()) {
-					saveToFile(filePath, getLines());
-				}
-				ConfigedMain.getMainFrame().deactivateLoadingCursor();
-			}
-
-			@Override
-			protected void saveAsZip() {
-				Logging.info(this, "saveAsZip");
-				ConfigedMain.getMainFrame().activateLoadingCursor();
-				String fileName = retrieveFileName(getInfo(), idents[i]);
-				String filePath = retrieveFilePath(fileName + ".zip");
-				if (filePath != null && !filePath.isEmpty()) {
-					saveToZipFile(filePath, fileName, getLines());
-				}
-				ConfigedMain.getMainFrame().deactivateLoadingCursor();
-			}
-
-			@Override
-			protected void saveAllAsZip() {
-				if (configedMain.getSelectedClients() == null || configedMain.getSelectedClients().isEmpty()) {
-					return;
-				}
-
-				Logging.info(this, "got ident " + idents[i]);
-
-				ConfigedMain.getMainFrame().activateLoadingCursor();
-				String fileName = retrieveFileName(getInfo(), ALL_LOGFILES_SUFFIX);
-				Logging.info(this, "retrieving file path");
-				String filePath = retrieveFilePath(fileName + ".zip");
-
-				if (filePath == null || filePath.isEmpty()) {
-					return;
-				}
-
-				Logging.info(this, "file path retrieved: " + filePath);
-
-				saveAllToZipFile(filePath, retrieveAllLogFiles());
-				ConfigedMain.getMainFrame().deactivateLoadingCursor();
-			}
-		};
-
-		textPanes[i] = showPane;
-
+	private void initLogTabComponent(int i, String defaultText) {
+		LogTabComponent logTabComponent = new LogTabComponent(defaultText, getFocusTraversalKeysEnabled(),
+				configedMain);
+		logTabComponent.setLogFileType(idents[i]);
+		textPanes[i] = logTabComponent;
 		super.addTab(idents[i], textPanes[i]);
 	}
 
-	private static String retrieveFileName(String clientName, String suffix) {
-		String fileName = suffix;
-		if (clientName != null) {
-			suffix = ALL_LOGFILES_SUFFIX.equals(suffix) ? ALL_LOGFILES_SUFFIX : (suffix + ".log");
-			fileName = clientName.replace('.', '_') + "___" + suffix;
-		}
-		return fileName;
-	}
-
-	private Map<String, String> retrieveAllLogFiles() {
-		Map<String, String> logfiles = new HashMap<>();
-		for (String ident : idents) {
-			Map<String, String> logfile = PersistenceControllerFactory.getPersistenceController().getLogDataService()
-					.getLogfile(configedMain.getSelectedClients().get(0), ident);
-			if (logfile.get(ident).split("\n").length > 1) {
-				logfiles.put(ident, logfile.get(ident));
-			}
-
-			Logging.info(this, "saveAllAsZip " + ident + " " + logfile.get(ident).split("\n").length);
-		}
-		return logfiles;
-	}
-
 	public void loadDocument(String ident) {
-		Logging.debug(this, "loadDocument ident " + ident);
-	}
-
-	private String retrieveFilePath(String typeName) {
-		String fileName = null;
-
-		if (chooser == null) {
-			setFileChooser("");
-			chooserDirectory = chooser.getCurrentDirectory();
-		}
-
-		File f = new File(chooserDirectory, typeName);
-		chooser.setSelectedFile(f);
-
-		int returnVal = chooser.showSaveDialog(ConfigedMain.getMainFrame());
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			fileName = chooser.getSelectedFile().getAbsolutePath();
-			chooserDirectory = chooser.getCurrentDirectory();
-		}
-
-		return fileName;
-	}
-
-	private void setFileChooser(String fileName) {
-		chooser = new JFileChooser(fileName);
-		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		chooser.setFileFilter(new FileNameExtensionFilter("logfiles: .log, .zip, .gz, .7z", "log", "zip", "gz", "7z"));
-		chooser.setDialogType(JFileChooser.SAVE_DIALOG);
-		chooser.setDialogTitle(Configed.getResourceValue("PanelTabbedDocument.saveFileChooser"));
-	}
-
-	private static void saveToFile(String fileName, String[] lines) {
-		try (FileWriter fWriter = new FileWriter(fileName, StandardCharsets.UTF_8)) {
-			for (int i = 0; i < lines.length; i++) {
-				fWriter.write(lines[i] + "\n");
-			}
-		} catch (IOException ex) {
-			Logging.error("Error writing to a file: " + fileName + "\n --- " + ex);
-		}
-	}
-
-	private void saveAllToZipFile(String filePath, Map<String, String> logfiles) {
-		try (ZipOutputStream out = new ZipOutputStream(new FileOutputStream(filePath))) {
-			out.setMethod(ZipOutputStream.DEFLATED);
-			for (String ident : idents) {
-				if (logfiles.get(ident) != null && logfiles.get(ident).split("\n").length > 1) {
-					String fileName = retrieveFileName(configedMain.getSelectedClients().get(0).replace(".", "_"),
-							ident);
-					ZipEntry entry = new ZipEntry(fileName);
-					out.putNextEntry(entry);
-					writeToOutputStream(logfiles.get(ident).split("\n"), out);
-				}
-			}
-		} catch (IOException ex) {
-			Logging.error("Error writing zip file: " + filePath + "\n --- " + ex);
-		}
-	}
-
-	private static void saveToZipFile(String filePath, String fileName, String[] lines) {
-		try (ZipOutputStream out = new ZipOutputStream(new FileOutputStream(filePath))) {
-			out.setMethod(ZipOutputStream.DEFLATED);
-			ZipEntry entry = new ZipEntry(fileName);
-			out.putNextEntry(entry);
-			writeToOutputStream(lines, out);
-		} catch (IOException ex) {
-			Logging.error("Error writing zip file: " + fileName + "\n --- " + ex);
-		}
-	}
-
-	private static void writeToOutputStream(String[] lines, ZipOutputStream out) {
-		for (int i = 0; i < lines.length; i++) {
-			try {
-				byte[] buffer = lines[i].getBytes(StandardCharsets.UTF_8);
-				out.write(buffer, 0, lines[i].length());
-				out.write(CRLF, 0, 2);
-			} catch (IOException ex) {
-				Logging.error("Error writing zip file: " + "\n --- " + ex);
-			}
-		}
+		LogTabComponent logTabComponent = (LogTabComponent) getTabComponentAt(getSelectedIndex());
+		logTabComponent.loadDocument(ident);
 	}
 
 	public void setDocuments(final Map<String, String> documents, final String info) {
