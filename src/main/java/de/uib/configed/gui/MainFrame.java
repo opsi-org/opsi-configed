ce1b719005ab78e200d9d108cd7603feb94fb8f8
otto <n.otto@uib.de>
2024-06-20 15:45:48 +0200

    [ref] Remove double code

--- src/main/java/de/uib/configed/gui/MainFrame.java
+++ src/main/java/de/uib/configed/gui/MainFrame.java
@@ -488,15 +488,19 @@ public class MainFrame extends JFrame {
 
 		jMenuHelp.addSeparator();
 
+		addCreditsMenus(jMenuHelp, this);
+
+		return jMenuHelp;
+	}
+
+	public static void addCreditsMenus(JMenu jMenuHelp, JFrame owner) {
 		JMenuItem jMenuHelpCredits = new JMenuItem(Configed.getResourceValue("MainFrame.jMenuHelpCredits"));
-		jMenuHelpCredits.addActionListener((ActionEvent e) -> FCreditsDialog.display(this));
+		jMenuHelpCredits.addActionListener((ActionEvent e) -> FCreditsDialog.display(owner));
 		jMenuHelp.add(jMenuHelpCredits);
 
 		JMenuItem jMenuHelpAbout = new JMenuItem(Configed.getResourceValue("MainFrame.jMenuHelpAbout"));
-		jMenuHelpAbout.addActionListener((ActionEvent e) -> Utils.showAboutAction(this));
+		jMenuHelpAbout.addActionListener((ActionEvent e) -> Utils.showAboutAction(owner));
 		jMenuHelp.add(jMenuHelpAbout);
-
-		return jMenuHelp;
 	}
 
 	public static void addLogfileMenus(JMenu jMenuHelp, JFrame centerFrame) {
