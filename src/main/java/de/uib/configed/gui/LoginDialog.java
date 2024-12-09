9d8e12a284c14e22474831fb59cbcb5a438ca8d0
otto <n.otto@uib.de>
2024-12-09 21:15:31 +0100

    [chg] Login without SSO when clicking enter in passwordField

--- src/main/java/de/uib/configed/gui/LoginDialog.java
+++ src/main/java/de/uib/configed/gui/LoginDialog.java
@@ -558,18 +558,17 @@ public class LoginDialog extends JFrame implements WaitingSleeper, KeyListener, 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-			if (ssoActiveByServer) {
+			if (e.getSource() == passwordField) {
+				tryConnecting(false);
+			} else if (Boolean.TRUE.equals(ssoActiveByServer)) {
 				tryConnecting(true);
+			} else if (e.getSource() == fieldHost.getEditor().getEditorComponent() || fieldUser.getText() == null
+					|| fieldUser.getText().isEmpty()) {
+				fieldUser.requestFocus();
+			} else if (passwordField.getPassword() == null || passwordField.getPassword().length == 0) {
+				passwordField.requestFocus();
 			} else {
-				if (e.getSource() == fieldHost.getEditor().getEditorComponent()) {
-					fieldUser.requestFocus();
-				} else if (fieldUser.getText() == null || fieldUser.getText().isEmpty()) {
-					fieldUser.requestFocus();
-				} else if (passwordField.getPassword() == null || passwordField.getPassword().length == 0) {
-					passwordField.requestFocus();
-				} else {
-					tryConnecting();
-				}
+				tryConnecting(false);
 			}
 		} else if (e.getKeyCode() == KeyEvent.VK_TAB) {
 			if (e.getSource() == fieldHost.getEditor().getEditorComponent()) {
