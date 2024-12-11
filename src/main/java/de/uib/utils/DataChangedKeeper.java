a7999f62d1da15a4e55ff9c964cb3be8e19cedd5
otto <n.otto@uib.de>
2024-12-11 23:10:18 +0100

    [ref] Remove unused code

--- src/main/java/de/uib/utils/DataChangedKeeper.java
+++ src/main/java/de/uib/utils/DataChangedKeeper.java
@@ -6,23 +6,15 @@
 
 package de.uib.utils;
 
-import java.util.function.Consumer;
-
 import de.uib.utils.logging.Logging;
 
 public class DataChangedKeeper implements DataChangedObserver {
 	protected boolean dataChanged;
 
-	private Consumer<Object> actUpon;
-
 	@Override
 	public void dataHaveChanged(Object source) {
 		Logging.debug(this, "dataHaveChanged ", source);
 		dataChanged = true;
-
-		if (actUpon != null) {
-			actUpon.accept(source);
-		}
 	}
 
 	public boolean isDataChanged() {
