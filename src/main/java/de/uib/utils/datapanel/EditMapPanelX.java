f288725b014e64f75d354b11d4e9eab0fb919a8e
otto <n.otto@uib.de>
2025-01-21 17:00:21 +0100

    [fix] Compile error

--- src/main/java/de/uib/utils/datapanel/EditMapPanelX.java
+++ src/main/java/de/uib/utils/datapanel/EditMapPanelX.java
@@ -213,7 +213,7 @@ public class EditMapPanelX extends DefaultEditMapPanel {
 		}
 	}
 
-	private JPopupMenu definePopup() {
+	protected JPopupMenu definePopup() {
 		Logging.info(this, "(EditMapPanelX) definePopup");
 
 		if (reloadable) {
