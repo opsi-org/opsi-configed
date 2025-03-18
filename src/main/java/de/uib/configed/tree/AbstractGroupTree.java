bfec55548390b5864aca5e6d31fa756156181b14
vidziunas <n.vidziunas@uib.de>
2025-03-18 09:25:27 +0100

    Revert "[chg] Don't notify of structure change"
    This reverts commit 445c3c9be0f0c386a14bc890aa703e7af64f96ed.

--- src/main/java/de/uib/configed/tree/AbstractGroupTree.java
+++ src/main/java/de/uib/configed/tree/AbstractGroupTree.java
@@ -134,6 +134,7 @@ public abstract class AbstractGroupTree extends JTree implements TreeSelectionLi
 		groups.clear();
 		rootNode.removeAllChildren();
 		groupNodeFullList.removeAllChildren();
+		model.nodeStructureChanged(groupNodeGroups);
 		createTree();
 
 		removeTreeSelectionListener(this);
