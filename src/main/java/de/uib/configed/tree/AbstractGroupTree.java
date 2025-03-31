8a5574635881e93673a46792733d8151d960af86
vidziunas <n.vidziunas@uib.de>
2025-03-31 11:49:01 +0200

    [chg] Search child based on name not index

--- src/main/java/de/uib/configed/tree/AbstractGroupTree.java
+++ src/main/java/de/uib/configed/tree/AbstractGroupTree.java
@@ -160,7 +160,8 @@ public abstract class AbstractGroupTree extends JTree implements TreeSelectionLi
 		Map<String, Map<String, Object>> expandedNodes = new HashMap<>();
 
 		Enumeration<TreePath> expanded = getExpandedDescendants(new TreePath(rootNode));
-		List<TreePath> selectionPaths = Arrays.asList(getSelectionPaths());
+		List<TreePath> selectionPaths = Arrays
+				.asList(getSelectionPaths() != null ? getSelectionPaths() : new TreePath[0]);
 
 		if (expanded != null) {
 			while (expanded.hasMoreElements()) {
@@ -180,8 +181,7 @@ public abstract class AbstractGroupTree extends JTree implements TreeSelectionLi
 				map.put("selected", true);
 				if (!((DefaultMutableTreeNode) path.getLastPathComponent()).getAllowsChildren()) {
 					map.put("parent", path.getParentPath().getLastPathComponent().toString());
-					map.put("index", model.getIndexOfChild(path.getParentPath().getLastPathComponent(),
-							path.getLastPathComponent()));
+					map.put("child", path.getLastPathComponent());
 				}
 				String parent = !((DefaultMutableTreeNode) path.getLastPathComponent()).getAllowsChildren()
 						? (path.getParentPath().getLastPathComponent().toString() + "/"
@@ -219,7 +219,8 @@ public abstract class AbstractGroupTree extends JTree implements TreeSelectionLi
 		if (result == null && node.getValue().containsKey("parent")) {
 			DefaultMutableTreeNode parentNode = groupNodes.get(node.getValue().get("parent"));
 			if (parentNode.getChildCount() > 0) {
-				result = (DefaultMutableTreeNode) parentNode.getChildAt((int) node.getValue().get("index"));
+				String child = node.getValue().get("child").toString();
+				result = getChildWithUserObjectString(child, parentNode);
 			}
 		}
 		return result;
