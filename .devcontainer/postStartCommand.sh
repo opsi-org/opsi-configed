25d8929064ea295da7b0cfd73c323f58cfebd19a
sucher <a.sucher@uib.de>
2025-06-04 09:11:03 +0000

    [fix] download opsi-dev-cli installer (not concrete version)

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -5,14 +5,11 @@ echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc  # for future shells
 source ~/.bashrc 
 
 echo "== Install dev-tools"
-MIN_VERSION=1.33.9 # will be updated to latest version, just indicate the required minimum version
-wget http://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/opsi-dev-tools_linux_x64_${MIN_VERSION}.tar.gz
-tar -xf opsi-dev-tools_linux_x64_${MIN_VERSION}.tar.gz
-mv opsi-dev-tool opsi-dev-cli
-./opsi-dev-cli self install # installs into ~/.local/bin
-rm -f opsi-dev-cli
-rm -f opsi-dev-tools_linux_x64_${MIN_VERSION}.tar.gz
+SCRIPT_NAME="installer.sh"
+DEV_TOOLS_URL=https://binaryindex.uib.gmbh/development/opsi-dev-tools/linux/x64/$SCRIPT_NAME
 
-
-opsi-dev-cli self upgrade # using script from ~/.local/bin/opsi-dev-tool
-opsi-dev-cli --version
+rm -f "./${SCRIPT_NAME}"
+wget $DEV_TOOLS_URL
+chmod 750 $SCRIPT_NAME && ./${SCRIPT_NAME}
+rm -f "./${SCRIPT_NAME}"
+rm -f "./${SCRIPT_NAME}*"
