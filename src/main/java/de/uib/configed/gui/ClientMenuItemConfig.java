a1edcda1fd481e24fef6d6f72d0f72e65746c396
vidziunas <n.vidziunas@uib.de>
2025-10-29 16:52:53 +0100

    [chg] Add header comment to ClientMenuItemConfig

--- src/main/java/de/uib/configed/gui/ClientMenuItemConfig.java
+++ src/main/java/de/uib/configed/gui/ClientMenuItemConfig.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) uib GmbH <info@uib.de>
+ * License: AGPL-3.0
+ * This file is part of opsi - https://www.opsi.org
+ */
+
 package de.uib.configed.gui;
 
 import java.util.function.Supplier;
@@ -6,17 +12,14 @@ import javax.swing.JMenu;
 
 public record ClientMenuItemConfig(String resourceKey, Runnable action, Supplier<JMenu> subMenuSupplier, String icon,
 		boolean invertedIcon, boolean dependOnSelectionCount, boolean readOnly) {
-	// Factory for normal menu items
 	public static ClientMenuItemConfig item(String resourceKey, Runnable action) {
 		return new ClientMenuItemConfig(resourceKey, action, null, null, false, false, false);
 	}
 
-	// Factory for submenus
 	public static ClientMenuItemConfig submenu(String resourceKey, Supplier<JMenu> supplier) {
 		return new ClientMenuItemConfig(resourceKey, null, supplier, null, false, false, false);
 	}
 
-	// Builder-style methods
 	public ClientMenuItemConfig withIcon(String icon) {
 		return new ClientMenuItemConfig(resourceKey, action, subMenuSupplier, icon, invertedIcon,
 				dependOnSelectionCount, readOnly);
