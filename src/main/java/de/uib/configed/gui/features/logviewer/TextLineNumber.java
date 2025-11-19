27454f73c951afe23eb2103f46b41e64284f1f8f
vidziunas <n.vidziunas@uib.de>
2025-11-19 11:29:16 +0100

    [ref] Reduce SonarQube warnings in TextLineNumber

--- src/main/java/de/uib/configed/gui/features/logviewer/TextLineNumber.java
+++ src/main/java/de/uib/configed/gui/features/logviewer/TextLineNumber.java
@@ -36,6 +36,8 @@ import javax.swing.text.JTextComponent;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.Utilities;
 
+import de.uib.configed.share.logging.Logging;
+
 /**
  * This class will display line numbers for a related text component. The text
  * component must use the same line height for each line. TextLineNumber
@@ -45,15 +47,17 @@ import javax.swing.text.Utilities;
  */
 // Original TextLineNumber class by tips4java
 // Source: https://github.com/tips4java/tips4java/blob/main/source/TextLineNumber.java
-// Modifications: Updated depracted method usage
-public class TextLineNumber extends JPanel implements CaretListener, DocumentListener, PropertyChangeListener {
-	public static final float LEFT = 0.0f;
-	public static final float CENTER = 0.5f;
-	public static final float RIGHT = 1.0f;
+// Modifications:
+//   - Updated depracted method usage.
+//   - Minor changes to reduce SonarQube warnings.
+public final class TextLineNumber extends JPanel implements CaretListener, DocumentListener, PropertyChangeListener {
+	public static final float LEFT = 0.0F;
+	public static final float CENTER = 0.5F;
+	public static final float RIGHT = 1.0F;
 
 	private static final MatteBorder OUTER = new MatteBorder(0, 0, 0, 2, Color.GRAY);
 
-	private static final int HEIGHT = Integer.MAX_VALUE - 1000000;
+	private static final int HEIGHT = Integer.MAX_VALUE - 100_0000;
 
 	//  Text component this TextTextLineNumber component is in sync with
 
@@ -71,7 +75,7 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 	//  needs to be repainted
 
 	private int lastDigits;
-	private double lastHeight;
+	private int lastHeight;
 	private int lastLine;
 
 	private HashMap<String, FontMetrics> fonts;
@@ -113,7 +117,7 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 	 *
 	 * @return the update font property
 	 */
-	public boolean getUpdateFont() {
+	public boolean updateFont() {
 		return updateFont;
 	}
 
@@ -191,10 +195,10 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 	 * @param digitalAlignement the horizontal alignment value
 	 */
 	public void setDigitAlignment(float digitAlignment) {
-		if (digitAlignment > 1.0f) {
-			this.digitAlignment = 1.0f;
+		if (digitAlignment > 1.0F) {
+			this.digitAlignment = 1.0F;
 		} else {
-			this.digitAlignment = digitAlignment < 0.0f ? -1.0f : digitAlignment;
+			this.digitAlignment = digitAlignment < 0.0F ? -1.0F : digitAlignment;
 		}
 	}
 
@@ -278,7 +282,8 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 				//  Move to the next row
 
 				rowStartOffset = Utilities.getRowEnd(component, rowStartOffset) + 1;
-			} catch (Exception e) {
+			} catch (BadLocationException e) {
+				Logging.error(this, "Failed to paint the component", e);
 				break;
 			}
 		}
@@ -298,7 +303,7 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 	 *	Get the line number to be drawn. The empty string will be returned
 	 *  when a line of text has wrapped.
 	 */
-	protected String getTextLineNumber(int rowStartOffset) {
+	private String getTextLineNumber(int rowStartOffset) {
 		Element root = component.getDocument().getDefaultRootElement();
 		int index = root.getElementIndex(rowStartOffset);
 		Element line = root.getElement(index);
@@ -313,6 +318,7 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 	/*
 	 *  Determine the X offset to properly align the line number when drawn
 	 */
+	@SuppressWarnings("java:S2164")
 	private int getOffsetX(int availableWidth, int stringWidth) {
 		return (int) ((availableWidth - stringWidth) * digitAlignment);
 	}
@@ -325,13 +331,12 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 
 		Rectangle2D r = component.modelToView2D(rowStartOffset);
 		int lineHeight = fontMetrics.getHeight();
-		int y = (int) (r.getY() + r.getHeight());
 		int descent = 0;
 
 		//  The text needs to be positioned above the bottom of the bounding
 		//  rectangle based on the descent of the font(s) contained on the row.
 
-		if (r.getHeight() == lineHeight) {
+		if (((int) r.getHeight()) == lineHeight) {
 			// default font is being used
 			descent = fontMetrics.getDescent();
 		} else {
@@ -351,7 +356,7 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 				Integer fontSize = (Integer) as.getAttribute(StyleConstants.FontSize);
 				String key = fontFamily + fontSize;
 
-				FontMetrics fm = fonts.computeIfAbsent(key, k -> {
+				FontMetrics fm = fonts.computeIfAbsent(key, (String k) -> {
 					Font font = new Font(fontFamily, Font.PLAIN, fontSize);
 					return component.getFontMetrics(font);
 				});
@@ -360,6 +365,7 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 			}
 		}
 
+		int y = (int) (r.getY() + r.getHeight());
 		return y - descent;
 	}
 
@@ -408,20 +414,22 @@ public class TextLineNumber extends JPanel implements CaretListener, DocumentLis
 		//  View of the component has not been updated at the time
 		//  the DocumentEvent is fired
 
-		SwingUtilities.invokeLater(() -> {
-			try {
-				int endPos = component.getDocument().getLength();
-				Rectangle2D rect = component.modelToView2D(endPos);
+		SwingUtilities.invokeLater(this::handleResizeIfNeeded);
+	}
 
-				if (rect != null && rect.getHeight() != lastHeight) {
-					setPreferredWidth();
-					getParent().repaint();
-					lastHeight = rect.getHeight();
-				}
-			} catch (BadLocationException ex) {
-				/* nothing to do */
+	private void handleResizeIfNeeded() {
+		try {
+			int endPos = component.getDocument().getLength();
+			Rectangle2D rect = component.modelToView2D(endPos);
+
+			if (rect != null && ((int) rect.getHeight()) != lastHeight) {
+				setPreferredWidth();
+				getParent().repaint();
+				lastHeight = (int) rect.getHeight();
 			}
-		});
+		} catch (BadLocationException ex) {
+			Logging.error(this, "Failed to repaint component", ex);
+		}
 	}
 
 	//
