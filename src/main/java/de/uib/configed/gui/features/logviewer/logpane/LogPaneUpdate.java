0ca2e22e5d34c126bff9c41eed7e06728136af73
vidziunas <n.vidziunas@uib.de>
2025-11-25 13:02:49 +0100

    [ref] Extract longer lambdas in LogPaneUpdate to methods

--- src/main/java/de/uib/configed/gui/features/logviewer/logpane/LogPaneUpdate.java
+++ src/main/java/de/uib/configed/gui/features/logviewer/logpane/LogPaneUpdate.java
@@ -18,35 +18,18 @@ public final class LogPaneUpdate {
 
 	}
 
+	@SuppressWarnings("java:S1541")
 	public static UpdateResult<LogPaneModel, LogPaneEffect> update(LogPaneMsg msg, LogPaneModel model) {
 		return switch (msg) {
 		case LogPaneMsg.SimpleMsg m -> handleSimpleMsg(m, model);
-		case LogPaneMsg.ParseLogRequested(String text, boolean resetCaret) -> {
-			int caretPos = resetCaret ? model.getCaretPosition() : 0;
-			yield UpdateResult.withEffect(model.toBuilder().logText(text).caretPosition(caretPos).build(),
-					LogPaneEffect.SimpleEffect.PARSE_LOG);
-		}
+		case LogPaneMsg.ParseLogRequested(String text, boolean resetCaret) -> handlePraseLogRequestedMsg(model, text,
+				resetCaret);
 		case LogPaneMsg.LogParsed(LogParsedData data, int level) -> UpdateResult
 				.noEffect(model.toBuilder().typesList(data.getTypesList()).minLevel(data.getMinExistingLevel())
 						.maxExistingLevel(data.getMaxExistingLevel()).showLevel(level).needsRebuild(true).build());
-		case LogPaneMsg.Search(String query) -> {
-			List<String> newHistory = new ArrayList<>(model.getSearchHistory());
-			if (!newHistory.contains(query)) {
-				newHistory.add(query);
-			}
-			yield UpdateResult.withEffect(model.withSearchHistory(newHistory),
-					LogPaneEffect.SimpleEffect.PERFORM_SEARCH);
-		}
-		case LogPaneMsg.ChangeLogLevel(int level) -> {
-			int oldShowLevel = model.getShowLevel();
-			boolean needsRebuild = oldShowLevel != level;
-			yield UpdateResult.noEffect(model.toBuilder().showLevel(level).needsRebuild(needsRebuild).build());
-		}
-		case LogPaneMsg.ChangeEventType(String type) -> {
-			String oldSelectedType = model.getSelectedType();
-			boolean needsRebuild = !type.equals(oldSelectedType);
-			yield UpdateResult.noEffect(model.toBuilder().selectedType(type).needsRebuild(needsRebuild).build());
-		}
+		case LogPaneMsg.Search(String query) -> handleSearchMsg(model, query);
+		case LogPaneMsg.ChangeLogLevel(int level) -> handleChangelogLevelMsg(model, level);
+		case LogPaneMsg.ChangeEventType(String type) -> handleChangeEventTypeMsg(model, type);
 		case LogPaneMsg.ChangeTitle(String title) -> UpdateResult.noEffect(model.withTitle(title));
 		case LogPaneMsg.ChangeInfo(String info) -> UpdateResult.noEffect(model.withInfo(info));
 		case LogPaneMsg.ChangeCaretPosition(int position) -> UpdateResult
@@ -54,6 +37,34 @@ public final class LogPaneUpdate {
 		case LogPaneMsg.ToggleCaseSensitivity(boolean caseSensitive) -> UpdateResult
 				.noEffect(model.withCaseSensitive(caseSensitive));
 		};
+	}
+
+	private static UpdateResult<LogPaneModel, LogPaneEffect> handleSearchMsg(LogPaneModel model, String query) {
+		List<String> newHistory = new ArrayList<>(model.getSearchHistory());
+		if (!newHistory.contains(query)) {
+			newHistory.add(query);
+		}
+		return UpdateResult.withEffect(model.withSearchHistory(newHistory), LogPaneEffect.SimpleEffect.PERFORM_SEARCH);
+	}
+
+	private static UpdateResult<LogPaneModel, LogPaneEffect> handleChangelogLevelMsg(LogPaneModel model, int level) {
+		int oldShowLevel = model.getShowLevel();
+		boolean needsRebuild = oldShowLevel != level;
+		return UpdateResult.noEffect(model.toBuilder().showLevel(level).needsRebuild(needsRebuild).build());
+	}
+
+	private static UpdateResult<LogPaneModel, LogPaneEffect> handleChangeEventTypeMsg(LogPaneModel model, String type) {
+		String oldSelectedType = model.getSelectedType();
+		boolean needsRebuild = !type.equals(oldSelectedType);
+		return UpdateResult.noEffect(model.toBuilder().selectedType(type).needsRebuild(needsRebuild).build());
+	}
+
+	private static UpdateResult<LogPaneModel, LogPaneEffect> handlePraseLogRequestedMsg(LogPaneModel model, String text,
+			boolean resetCaret) {
+		int caretPos = resetCaret ? model.getCaretPosition() : 0;
+		return UpdateResult.withEffect(model.toBuilder().logText(text).caretPosition(caretPos).build(),
+				LogPaneEffect.SimpleEffect.PARSE_LOG);
+
 	}
 
 	private static UpdateResult<LogPaneModel, LogPaneEffect> handleSimpleMsg(LogPaneMsg.SimpleMsg msg,
