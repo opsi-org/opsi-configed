423f972a00a856193868b3427870d6b1ca0ac8db
vidziunas <n.vidziunas@uib.de>
2026-04-13 15:12:36 +0200

    [chg] Update header

--- src/main/java/de/uib/configed/core/infrastructure/webdav/WebDAVClient.java
+++ src/main/java/de/uib/configed/core/infrastructure/webdav/WebDAVClient.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) UIB GmbH <info@uib.de>
  * License: AGPL-3.0
- * This file is part of opsi - https://www.opsi.org
+ * This file is part of OPSI - https://www.opsi.org
  */
 
 package de.uib.configed.core.infrastructure.webdav;
