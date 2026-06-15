28081677720c80200e92a9535224204e276edf59
vidziunas <n.vidziunas@uib.de>
2026-06-15 12:14:54 +0200

    [ref] Remove empty line

--- src/test/java/de/uib/configed/gui/features/searchpane/SearchCriteriaEngineTest.java
+++ src/test/java/de/uib/configed/gui/features/searchpane/SearchCriteriaEngineTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class SearchCriteriaEngineTest {
-
 	private final SearchCriteriaEngine engine = new SearchCriteriaEngine();
 
 	// --- Pattern Compilation Tests ---
