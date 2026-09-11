b8a8ec6a9f76a0ab17df5608492725042939bc3d
vidziunas <n.vidziunas@uib.de>
2026-09-11 17:51:29 +0200

    [fix,pub] In client table toggle columns not working properly (via context menu)

--- src/main/java/de/uib/configed/gui/ConfigedMain.java
+++ src/main/java/de/uib/configed/gui/ConfigedMain.java
@@ -252,7 +252,7 @@ public class ConfigedMain {
 	public void toggleColumn(String column) {
 		Map<String, Boolean> fields = persistenceController.getDataServices().host.getHostDisplayFields();
 		fields.put(column, !fields.get(column));
-		setRebuiltClientListTableModel(true, false);
+		clientTablePanel.getTableComponent().dispatch(new GenericTableViewMsg.ToggleColumn(column));
 	}
 
 	public void handleGroupActionRequest() {
